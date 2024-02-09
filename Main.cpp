d29b9e7b8b47d77c313149da2287f089db7d912f
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-02-09 16:38:06 -0500

    Update Main.cpp
    Finish binary search tree creation

--- Main.cpp
+++ Main.cpp
@@ -10,12 +10,16 @@ int main(){
     ifstream in("words.txt");
     wTree* wds=new wTree;
     string tmp;
+    cout<<"creating binary search tree...\nappending words to binary search tree...\nThis may take some time\n";
     while (in>>tmp){
         wTree* nd=wds;
         for (int i=0;i<tmp.length();i++){
             if (!nd->hNode[tmp[i]-'a']){
                 nd->hNode[tmp[i]-'a']=true;
+                nd->btchs[tmp[i]-'a']=new wTree;
             }
+            nd=btchs[tmp[i]-'a'];
         }
+        nd->isW=true;
     }
 }
