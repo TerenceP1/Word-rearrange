e147c9b0a6497c19f41705dc1f5af0145636a0b2
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-02-09 15:21:10 -0500

    Update Main.cpp
    Starting the binary search tree creation.

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+struct wTree{
+    bool isW=false;
+    bool hNode[26]={false};
+    wTree* btchs[26];
+};
+int main(){
+    ifstream in("words.txt");
+    wTree* wds=new wTree;
+    string tmp;
+    while (in>>tmp){
+        wTree* nd=wds;
+        for (int i=0;i<tmp.length();i++){
+            if (!nd->hNode[tmp[i]-'a']){
+            }
+        }
+    }
+}
